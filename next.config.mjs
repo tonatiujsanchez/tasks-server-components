48329a28fb29765106f8baf453837bc842b2b35b
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2024-07-27 13:43:17 -0600

    revalidate, removido

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        staleTimes: {
-            dynamic: 0
-        }
-    },
     images: {
         remotePatterns: [
             {
--- src/app/dashboard/server-tasks/page.tsx
+++ src/app/dashboard/server-tasks/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
 import { TaskList, TasksHeader } from "@/tasks";
 import prisma from "@/lib/prisma";
 
